c7701056d947dac7135e568c556507be5296344d
Abhideep Mallick <androabhideep@gmail.com>
2024-11-13 19:47:29 +0530

    Added comments to main layout screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"; // Import Stack for stack-based navigation layout
 
+// Define RootLayout component to manage the overall stack navigation structure
 export default function RootLayout() {
   return (
     <Stack>
-       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* Define a screen within the stack navigation */}
+      <Stack.Screen 
+        name="(tabs)" // Specify the name of the screen, linking it to the "(tabs)" route
+        options={{ headerShown: false }} // Hide the header for this screen
+      />
     </Stack>
   );
 }
